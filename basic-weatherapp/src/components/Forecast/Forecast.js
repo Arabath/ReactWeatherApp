a3337ca6a0c492233a0af1cdc63bea77b7824841
Axel Hobrecht <axelhobrecht@hotmail.com>
2022-04-16 18:41:10 -0300

    Weather app develop starts

---
+++ basic-weatherapp/src/components/Forecast/Forecast.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import Conditions from "../Conditions/Conditions";
+
+const Forecast = () => {
+  let [city, setCity] = useState("");
+  let [unit, setUnit] = useState("imperial");
+  let [responseObj, setResponseObj] = useState({});
+  const uriEncodedCity = encodeURIComponent(city);
+  function getForecast(e) {
+    e.preventDefault();  
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com",
+        "X-RapidAPI-Key": "0b70c7a579msh242db71b8cbca31p1a45cdjsn264d0e1bce97",
+      },
+    };
+
+    fetch(
+      `https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        setResponseObj(response);
+      });
+  }
+  return (
+    <div>
+      <h2>Find Current Weather Conditions</h2>
+      <form onSubmit={getForecast}>
+        <input
+          type="text"
+          placeholder="Enter City"
+          maxLength="50"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <label>
+          <input
+            type="radio"
+            name="units"
+            checked={unit === "imperial"}
+            value="imperial"
+            onChange={(e) => setUnit(e.target.value)}
+          />
+          Fahrenheit
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="units"
+            checked={unit === "metric"}
+            value="metric"
+            onChange={(e) => setUnit(e.target.value)}
+          />
+          Celcius
+        </label>
+        <button type="submit">Get Forecast</button>
+      </form>
+      <Conditions responseObj={responseObj} />
+    </div>
+  );
+};
+export default Forecast;
